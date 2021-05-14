730611588356d9b6e02a62b3a3ce81d22fdd1880
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-14 15:23:50 +0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAf4Jy987bE9PE5NLFB7oL1UTvHvWMFVUE",
-    authDomain: "chatroom-826ea.firebaseapp.com",
-    projectId: "chatroom-826ea",
-    storageBucket: "chatroom-826ea.appspot.com",
-    messagingSenderId: "1097497890314",
-    appId: "1:1097497890314:web:e50648cf5f49af436199f3",
-    measurementId: "G-0GRFH89QD9"
+    apiKey: "KEY",
+    authDomain: "KEY",
+    projectId: "KEY",
+    storageBucket: "KEY",
+    messagingSenderId: "KEY",
+    appId: "KEY",
+    measurementId: "KEY"
 });
 
 const db = firebaseApp.firestore();
